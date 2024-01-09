307450dc3a0532d4422ce05a0d7faba9232d737b
Jason Marston <jason@jasonmarston.me.uk>
2024-01-09 15:09:01 +0000

    Istio and Kubernetes configuration

--- framework-input/src/main/java/module-info.java
+++ framework-input/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module framework.input {
 	requires application.input;
 	requires static lombok;
+	requires org.slf4j;
 	requires jakarta.cdi;
 	requires jakarta.ws.rs;
 	requires io.smallrye.mutiny;
